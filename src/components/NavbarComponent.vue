57607c6e31de286a213e10a6cc7ff8cc0fb5f9af
PochienTsai <bernie7135@gmail.com>
2024-03-09 22:02:21 +0800

    加入購物車範例

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="navbar bg-light">
+    <div class="container-fluid">
+      <span class="navbar-brand" href="#">香香麵攤</span>
+      <button type="button" class="btn nav-link">
+        購物車
+        <span class="badge rounded-pill bg-danger text-white">{{ cartList.carts.length }}</span>
+      </button>
+    </div>
+  </nav>
+</template>
+<script>
+import cartStore from '../stores/cartStore.js'
+export default {
+  computed: {
+    cartList() {
+      return cartStore().cartList
+    }
+  }
+}
+</script>
